3fc3e8819dc803fc199e19e25d0f6f282d9c9c2a
Matt Toensing <matthew.toensing@gmail.com>
2021-05-17 20:39:46 -0600

    Create instuction method

--- lib/game.rb
+++ lib/game.rb
@@ -1,11 +1,47 @@
 class Game
 
-  def welcome
-    p 'Welcome to MASTERMIND\n'
-    p 'Would you like to (p)lay, read the (i)instructions, or (q)uit?'
-    user_input = gets.chomp
+  # def hash
+  #   hash = {'p' => play,
+  #     'i' => instructions,
+  #     'q' => quit,
+  #     'c' => cheat
+  #   }
+  # end
 
-    require 'pry'; binding.pry
+  def initialize
+
+  end
+
+  def welcome
+    # require "pry"; binding.pry
+    p 'Welcome to MASTERMIND\n'
+    p 'Would you like to (p)lay, read the (i)nstructions, or (q)uit?'
+    user_input = gets.chomp
+    if user_input == 'i'
+      instructions
+    elsif user_input == 'p'
+      play
+    elsif user_input == 'q'
+      quit
+    elsif user_input == 'c'
+      cheat
+    end
+  end
+
+  def instructions
+    p 'here are the instructions'
+  end
+
+  def play
+    p 'play game'
+  end
+
+  def quit
+    p 'Maybe next time'
+  end
+
+  def cheat
+    p "Can't do it on your own, huh?"
   end
 
 end
