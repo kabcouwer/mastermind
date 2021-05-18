79eb6231b85cada90537e87d3c238c566cd048c4
Kim Abcouwer <kimberly.abcouwer@gmail.com>
2021-05-18 17:27:16 -0600

    Create a code method and change secret code sytax from repeating itself

--- lib/secret_code.rb
+++ lib/secret_code.rb
@@ -1,9 +1,9 @@
 class SecretCode
-  attr_reader :pegs, :secret_code
+  attr_reader :pegs, :code
 
   def initialize
     @pegs = []
-    @secret_code = []
+    @code = []
   end
 
   def add_peg(peg)
@@ -14,13 +14,32 @@ class SecretCode
     @pegs.shuffle!
   end
 
-  def row
-    @pegs.each_with_index do |peg, index|
-      until index == 3
-      @secret_code << peg
-      break
-    end 
+  def row_one
+    @pegs.find do |peg|
+      peg.position == 1
     end
-    @secret_code
   end
+
+  def row_two
+    @pegs.find do |peg|
+      peg.position == 2
+    end
+  end
+
+  def row_three
+    @pegs.find do |peg|
+      peg.position == 3
+    end
+  end
+
+  def row_four
+    @pegs.find do |peg|
+      peg.position == 4
+    end
+  end
+
+  def create_code
+    @code = [row_one, row_two, row_three, row_four]
+  end
+
 end
