ecb2cadc6d321ee3822c492d263bd55718ce7297
Kim Abcouwer <kimberly.abcouwer@gmail.com>
2021-05-19 08:55:50 -0600

    start to fix compare_blue method and correct color counter and changed code to solution

--- lib/play.rb
+++ lib/play.rb
@@ -1,10 +1,10 @@
 
 class Play
-  attr_reader :guess, :code, :start_game
+  attr_reader :guess, :solution, :start_game
 
-  def initialize(code)
+  def initialize(solution)
     @guess = []
-    @code = code
+    @solution = solution
     @guess_counter = 0
     @correct_colors = 0
     @correct_positions = 0
@@ -29,67 +29,85 @@ class Play
   end
 
   def compare_blue
-    require 'pry'; binding.pry
-    guess_blue = @guess.count('b')
-    code_blue = @code.code.color.count(‘b’)
-    if guess_blue == 0
-      #compare_green
-      compare_first_position
-    elsif guess_blue == code_blue
-      @correct_colors += code_blue
-    elsif guess_blue > code_blue
-      until guess_blue == code_blue
-        guess_blue - 1
-        # break
-      end
-      @correct_colors += code_blue
-    elsif guess_blue < code_blue
-      until guess_blue == code_blue
-        code_blue - 1
-      end
-      @correct_colors += code_blue
+    if @guess.include?('b')
+      # @correct_colors += @solution.solution.count('b')
+      @solution.solution.each do |peg|
+        return true if peg.color.include?('b')
+        require 'pry'; binding.pry
+          # @correct_colors += 1
+        end
+      p 'code contains blue'
+    else
+      p 'code does not contain blue'
     end
-    #compare_green
     compare_first_position
   end
+
+  # def compare_green
+  #   if @guess.include?('g')
+  #     correct_colors += @code.code.color.count('g')
+  #   end
+  # end
+
+  # def compare_blue
+  #   require 'pry'; binding.pry
+  #   guess_blue = @guess.count('b')
+  #   code_blue = @code.code.color.count('b')
+  #   if guess_blue == 0
+  #     #compare_green
+  #     compare_first_position
+  #   elsif guess_blue == code_blue
+  #     @correct_colors += code_blue
+  #   elsif guess_blue > code_blue
+  #     until guess_blue == code_blue
+  #       guess_blue - 1
+  #       # break
+  #     end
+  #     @correct_colors += code_blue
+  #   elsif guess_blue < code_blue
+  #     until guess_blue == code_blue
+  #       code_blue - 1
+  #     end
+  #     @correct_colors += code_blue
+  #   end
+  #   #compare_green
+  #   compare_first_position
+  # end
 
     # @guess.count(‘b’) == @code.code.count(‘b’)
 
   def compare_first_position
-    if @guess[0] == @code.code[0].color
+    if @guess[0] == @solution.solution[0].color
       p 'first position is correct'
-      compare_second_position
       @correct_positions += 1
     else
       p 'first position not correct'
-      compare_second_position
     end
+    compare_second_position
   end
 
   def compare_second_position
-    if @guess[1] == @code.code[1].color
+    if @guess[1] == @solution.solution[1].color
       p 'second position is correct'
-      compare_third_position
       @correct_positions += 1
     else
       p 'second position not correct'
-      compare_third_position
     end
+    compare_third_positon
   end
 
   def compare_third_positon
-    if @guess[2] == @code.code[2].color
+    if @guess[2] == @solution.solution[2].color
       p 'third position is correct'
-      compare_fourth_position
       @correct_positions += 1
     else
       p 'third position not correct'
-      compare_fourth_position
     end
+    compare_fourth_position
   end
 
   def compare_fourth_position
-    if @guess[3] == @code.code[3].color
+    if @guess[3] == @solution.solution[3].color
       p 'fourth position is correct'
       @correct_positions += 1
     else
