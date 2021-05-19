425a8c6ab5436602400ae268f05afd12f6b4c406
Matt Toensing <matthew.toensing@gmail.com>
2021-05-18 19:47:30 -0600

    Change color from symbol to string

--- spec/play_spec.rb
+++ spec/play_spec.rb
@@ -2,14 +2,14 @@ require_relative 'spec_helper'
 
 RSpec.describe Play do
   before :each do
-    @peg1 = Peg.new(:blue, 4)
-    @peg2 = Peg.new(:red, 3)
-    @peg3 = Peg.new(:green, 2)
-    @peg4 = Peg.new(:yellow, 1)
-    @peg5 = Peg.new(:blue, 1)
-    @peg6 = Peg.new(:red, 2)
-    @peg7 = Peg.new(:green, 3)
-    @peg8 = Peg.new(:yellow, 4)
+    @peg1 = Peg.new('b', 4)
+    @peg2 = Peg.new('r', 3)
+    @peg3 = Peg.new('g', 2)
+    @peg4 = Peg.new('y', 1)
+    @peg5 = Peg.new('b', 1)
+    @peg6 = Peg.new('r', 2)
+    @peg7 = Peg.new('g', 3)
+    @peg8 = Peg.new('y', 4)
     @code1 = SecretCode.new
     @code1.add_peg(@peg1)
     @code1.add_peg(@peg2)
@@ -38,6 +38,10 @@ RSpec.describe Play do
   end
 
   it 'test start game to message' do
-    expect(@play.start_game).to be_a(String)
+    expect(@play.start_game).to be_a(Array)
+  end
+
+  it "compare element" do
+    @play.compare_first_element
   end
 end
