ee34ea4c9c99ecea3aaad1259b94a4479fc66705
Kim Abcouwer <kimberly.abcouwer@gmail.com>
2021-05-23 16:41:52 -0600

    Add testing for instructions message - still needs work

--- spec/mastermind_spec.rb
+++ spec/mastermind_spec.rb
@@ -13,6 +13,10 @@ RSpec.describe Mastermind do
     expect(@mastermind.turn).to be_an_instance_of(Turn)
   end
 
+  it 'creates a solution and is an array' do
+    expect(@mastermind.solution.class).to eq(Array)
+  end
+
   xit 'can display messages' do
     expect(@mastermind.welcome_screen).to be_a(String)
   end
@@ -20,7 +24,9 @@ RSpec.describe Mastermind do
   context 'player choice can print instructions' do
 
     it 'can display instructions' do
-      allow(@mastermind.player_choice).to receive(:i).and_return("MASTERMIND Game Rules\r\nOBJECT OF THE GAME\r\nThe object of MASTERMIND is to guess a secret code consisting of a series of 4 colored <pegs>. Each guess results in feedback narrowing down the possibilities of the code. You win the game by solving the secret code.\r\nTO BEGIN\r\nWhen prompted, enter your first guess, a sequence with four elements made up of: (r)ed, (g)reen, (b)lue, and (y)ellow. For example: rrgb\r\nNow that you have read the instructions, would you like to (p)lay or (q)uit?")
+      allow(@mastermind).to receive(:gets).and_return("i")
+
+      expect(@mastermind.player_choice).to eq("MASTERMIND Game Rules\r\nOBJECT OF THE GAME\r\nThe object of MASTERMIND is to guess a secret code consisting of a series of 4 colored <pegs>. Each guess results in feedback narrowing down the possibilities of the code. You win the game by solving the secret code.\r\nTO BEGIN\r\nWhen prompted, enter your first guess, a sequence with four elements made up of: (r)ed, (g)reen, (b)lue, and (y)ellow. For example: rrgb\r\nNow that you have read the instructions, would you like to (p)lay or (q)uit?")
     end
   end
 
