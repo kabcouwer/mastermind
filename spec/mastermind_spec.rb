dcd5cbd5faf564fad001be219c21add80b4267a0
Kim Abcouwer <kimberly.abcouwer@gmail.com>
2021-05-24 18:16:14 -0600

    Add testing but still needs work

--- spec/mastermind_spec.rb
+++ spec/mastermind_spec.rb
@@ -7,24 +7,29 @@ RSpec.describe Mastermind do
 
   it 'exists' do
     expect(@mastermind).to be_an_instance_of(Mastermind)
-    require "pry"; binding.pry
   end
 
-  xit 'can initialize with other classes' do
+  it 'can initialize with other classes' do
     expect(@mastermind.turn).to be_an_instance_of(Turn)
+  end
+
+  xit 'can create a new code after playing a round' do
+    mastermind1 = Mastermind.new
+    # require 'pry'; binding.pry
+    first_round_code = mastermind1.solution
+    mastermind1.play_again
+    next_round_code = mastermind1.solution
+    require 'pry'; binding.pry
+    expect(first_round_code).to not_equal(next_round_code)
   end
 
   xit 'can display messages' do
     expect(@mastermind.welcome_screen.class).to eq(String)
   end
 
-  it "text" do
-
-  end
-
   context 'player choice can print instructions' do
 
-    it 'can display instructions' do
+    xit 'can display instructions' do
       allow(@mastermind).to receive(:player_choice).and_return(@mastermind.instructions)
 
       expect(@mastermind.player_choice).to eq("MASTERMIND Game Rules\r\nOBJECT OF THE GAME\r\nThe object of MASTERMIND is to guess a secret code consisting of a series of 4 colored <pegs>. Each guess results in feedback narrowing down the possibilities of the code. You win the game by solving the secret code.\r\nTO BEGIN\r\nWhen prompted, enter your first guess, a sequence with four elements made up of: (r)ed, (g)reen, (b)lue, and (y)ellow. For example: rrgb\r\nNow that you have read the instructions, would you like to (p)lay or (q)uit?")
@@ -33,7 +38,7 @@ RSpec.describe Mastermind do
 
   context 'player choice can quit game' do
 
-    it 'quit game' do
+    xit 'quit game' do
       allow(@mastermind).to receive(:player_choice).and_return("q")
     end
   end
